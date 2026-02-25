0b5f8f8aa1a4344876c3e65e0289b37a8144a53a
Michael Eisenstein <michael@eave.media>
2026-02-25 23:08:05 +0000

    Update robots to discourage indexing of ?view= URLs

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,18 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://rankf1.com";
-
   return {
     rules: [
       {
         userAgent: "*",
         allow: "/",
+        disallow: [
+          // Prevent crawling of view-based query entry points now that we have clean URLs
+          "/*?view=",
+          "/*&view=",
+        ],
       },
     ],
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: "https://rankf1.com/sitemap.xml",
   };
 }
