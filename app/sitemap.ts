86fd6f5d9ff921937ad5f558a5364777d41cc77c
Michael Eisenstein <michael@eave.media>
2026-02-25 22:45:29 +0000

    Add /quiz and /global to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,6 +11,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: `${baseUrl}/quiz`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/global`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: "monthly",
